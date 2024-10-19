794142fdeb02f94a7342b89478b0aa679746fa30
imvinayak32 <vinayaksrocksrock@gmail.com>
2024-10-19 12:08:19 +0530

    added component>Navbar.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
-    <div className="text-3xl font-bold underline">App</div>
+<>
+<Navbar />
+</>
   )
 }
 
